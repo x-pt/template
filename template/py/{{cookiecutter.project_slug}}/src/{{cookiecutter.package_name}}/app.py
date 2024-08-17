a587fd2e68f1df5c0d247b4f2548cd0464b7f782
Mystic <215104920@qq.com>
2024-08-17 23:21:42 +0800

    feat(py): update Dockerfile and compose

--- template/py/{{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/app.py
+++ template/py/{{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/app.py
@@ -1,2 +1,16 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+def run_server(port=8000):
+    handler = SimpleHTTPRequestHandler
+    with HTTPServer(("", port), handler) as httpd:
+        print(f"Serving at port {port}")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down the server...")
+            httpd.shutdown()
+
+
 if __name__ == "__main__":
-    print("hello, world")
+    run_server()
