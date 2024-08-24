7fdc61bcd318b7b1fd733cc362e79543bf56f1ad
Mystic <215104920@qq.com>
2024-08-24 16:26:53 +0800

    style: remove the redundant blank lines

--- template/cxx/{{cookiecutter.project_slug}}/xmake.lua
+++ template/cxx/{{cookiecutter.project_slug}}/xmake.lua
@@ -2,7 +2,7 @@
 set_project("{{cookiecutter.project_slug}}")
 set_languages("c++{{cookiecutter.cxx_standard_version}}")
 
-{% if cookiecutter.cxx_project_type == "binary" %}
+{% if cookiecutter.cxx_project_type == "binary" -%}
 -- Binary project setup
 target("{{cookiecutter.project_slug}}")
     set_targetdir("build/bin")
@@ -10,18 +10,18 @@ target("{{cookiecutter.project_slug}}")
     add_files("src/*.cpp")
     add_headerfiles("src/*.h")
 
-    {% if cookiecutter.cxx_share_enabled == "STATIC" %}
+    {%- if cookiecutter.cxx_share_enabled == "STATIC" -%}
     -- Static link settings for non-macOS platforms
     if not is_plat("macosx") then
         add_ldflags("-static", {force = true})
     end
-    {% endif %}
+    {%- endif %}
 
-{% else %}
+{%- else -%}
 -- Library project setup
 target("{{cookiecutter.project_slug}}")
     set_targetdir("build/lib")
     set_kind("{{cookiecutter.cxx_share_enabled | lower}}")
     add_files("src/*.cpp")
     add_headerfiles("src/*.h")
-{% endif %}
+{%- endif %}
