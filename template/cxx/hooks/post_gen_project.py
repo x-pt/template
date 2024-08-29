d41eb0749b47c4351e0590445eebe125174d6788
Mystic <215104920@qq.com>
2024-08-29 17:02:05 +0800

    feat(cxx): git init at post_gen_project

--- template/cxx/hooks/post_gen_project.py
+++ template/cxx/hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 cxx_build_tool = "{{cookiecutter.cxx_build_tool}}"
 
@@ -13,3 +14,11 @@ elif cxx_build_tool == "xmake":
         os.remove(cmake_file)
 else:
     raise ValueError(f"Unknown cxx_build_tool: {cxx_build_tool}")
+
+# Initialize Git repository
+try:
+    subprocess.run(["git", "init"], check=True)
+except subprocess.CalledProcessError:
+    print("Failed to initialize Git repository. Please make sure Git is installed and try manually.")
+except FileNotFoundError:
+    print("Git command not found. Please install Git and try again.")
