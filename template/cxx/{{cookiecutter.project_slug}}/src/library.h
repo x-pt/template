8b11c1fdd917e3e4e886c777e5ef6df0dd7247c4
Mystic <215104920@qq.com>
2024-09-06 17:44:39 +0800

    feat(cxx): introduce google test

--- template/cxx/{{cookiecutter.project_slug}}/src/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef {{cookiecutter.package_name | upper}}_LIBRARY_H
-#define {{cookiecutter.package_name | upper}}_LIBRARY_H
-
-void hello();
-
-#endif //{{cookiecutter.package_name | upper}}_LIBRARY_H
--- template/cxx/{{cookiecutter.project_slug}}/xmake.lua
+++ template/cxx/{{cookiecutter.project_slug}}/xmake.lua
@@ -2,26 +2,21 @@
 set_project("{{cookiecutter.project_slug}}")
 set_languages("c++{{cookiecutter.cxx_standard_version}}")
 
-{% if cookiecutter.cxx_project_type == "binary" -%}
--- Binary project setup
+-- Include directories
+add_includedirs("include")
+
+-- Add targets
+target("{{cookiecutter.package_name}}_lib")
+    set_targetdir("build/lib")
+    set_kind("static")
+    add_files("src/*.cpp")
+    add_headerfiles("include/*.h")
+
 target("{{cookiecutter.project_slug}}")
     set_targetdir("build/bin")
     set_kind("binary")
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
+    add_files("src/main.cpp")
+    add_deps("{{cookiecutter.package_name}}_lib")
 
-    {%- if cookiecutter.cxx_share_enabled == "STATIC" -%}
-    -- Static link settings for non-macOS platforms
-    if not is_plat("macosx") then
-        add_ldflags("-static", {force = true})
-    end
-    {%- endif %}
-
-{%- else -%}
--- Library project setup
-target("{{cookiecutter.project_slug}}")
-    set_targetdir("build/lib")
-    set_kind("{{cookiecutter.cxx_share_enabled | lower}}")
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
-{%- endif %}
+-- Add tests
+includes("tests")
