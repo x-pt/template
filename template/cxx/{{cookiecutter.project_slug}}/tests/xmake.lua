9c3c6b020b2f8250c77adba22b45c8a2110ddda5
Mystic <215104920@qq.com>
2024-09-06 21:18:55 +0800

    ci(cxx): add test stage at github action

--- template/cxx/{{cookiecutter.project_slug}}/tests/xmake.lua
+++ template/cxx/{{cookiecutter.project_slug}}/tests/xmake.lua
@@ -3,6 +3,7 @@ add_requires("gtest")
 
 -- Add test target
 target("{{cookiecutter.project_slug}}-tests")
+    set_targetdir("$(buildir)/bin")
     set_kind("binary")
     add_files("*.cpp")
     add_deps("{{cookiecutter.package_name}}_lib")
