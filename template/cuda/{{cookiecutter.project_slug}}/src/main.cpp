645975fab379a0fc9c9e20dde2a0da8f2b2ae438
Mystic <215104920@qq.com>
2024-09-10 00:21:28 +0800

    test(cuda): add more test cases

--- template/cuda/{{cookiecutter.project_slug}}/src/main.cpp
+++ template/cuda/{{cookiecutter.project_slug}}/src/main.cpp
@@ -7,5 +7,7 @@ int main()
     say_hi();
     std::cout << "add(1, 2) = " << add(1, 2) << std::endl;
     std::cout << "sub(1, 2) = " << sub(1, 2) << std::endl;
+    std::cout << "mul(1, 2) = " << mul(1, 2) << std::endl;
+    std::cout << "divide(1, 2) = " << divide(1, 2) << std::endl;
     return 0;
 }
--- template/cuda/{{cookiecutter.project_slug}}/src/my_lib.cpp
+++ template/cuda/{{cookiecutter.project_slug}}/src/my_lib.cpp
@@ -9,3 +9,17 @@ int sub(const int a, const int b)
 {
     return a - b;
 }
+
+int mul(const int a, const int b)
+{
+    return a * b;
+}
+
+double divide(const double a, const double b)
+{
+    if (b == 0.0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
+    return a / b;
+}
