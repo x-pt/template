2cbcded096490a1c1f3c7818617aea8c80b84e4d
Mystic <215104920@qq.com>
2024-11-25 14:44:00 +0800

    feat(cxx): improve XXX package finding and support version detection
    - Add early return if target is already defined
    - Enhance search paths for include directories and libraries
    - Implement version detection from xxx_version.h
    - Create imported target XXX::XXX for easier usage
    - Improve code readability and maintainability

--- template/cxx/{{cookiecutter.project_slug}}/cmake/FindXXX.cmake
+++ template/cxx/{{cookiecutter.project_slug}}/cmake/FindXXX.cmake
@@ -1,10 +1,15 @@
 # FindXXX.cmake - Locate XXX library and headers
 #
 # This module defines the following variables:
-#   XXX_FOUND        - True if the XXX library and headers are found
-#   XXX_INCLUDE_DIRS - The include directories for XXX
-#   XXX_LIBRARIES    - The libraries to link against for XXX
-#   XXX_VERSION      - The version string of XXX (if available)
+#   XXX_FOUND          - True if the XXX library and headers are found
+#   XXX_INCLUDE_DIRS   - The include directories for XXX
+#   XXX_LIBRARIES      - The libraries to link against for XXX
+#   XXX_VERSION        - The version string of XXX (if available)
+
+# Early return if target is already defined
+if(TARGET XXX::XXX)
+    return()
+endif()
 
 # Define search paths
 set(XXX_SEARCH_PATHS
@@ -14,32 +19,30 @@ set(XXX_SEARCH_PATHS
         /usr
         /opt/xxx
         /opt
+        # Add other common installation paths here
 )
 
 # Locate the header files
-find_path(XXX_INCLUDE_DIR NAMES xxx.h
-        HINTS
-        ${XXX_ROOT}
-        $ENV{XXX_ROOT}
-        PATHS
-        ${XXX_SEARCH_PATHS}
+find_path(XXX_INCLUDE_DIR
+        NAMES xxx.h
+        HINTS ${XXX_ROOT} $ENV{XXX_ROOT}
+        PATHS ${XXX_SEARCH_PATHS}
         PATH_SUFFIXES include
+        DOC "XXX include directory"
 )
 
 # Define library components
-set(XXX_LIB_COMPONENTS component1 component2 component3)
+set(XXX_LIB_COMPONENTS xxx)  # Add more components if needed
 
 # Locate the libraries
 set(XXX_LIBRARIES)
 foreach(_comp ${XXX_LIB_COMPONENTS})
     find_library(XXX_${_comp}_LIBRARY
             NAMES ${_comp}
-            HINTS
-            ${XXX_ROOT}
-            $ENV{XXX_ROOT}
-            PATHS
-            ${XXX_SEARCH_PATHS}
+            HINTS ${XXX_ROOT} $ENV{XXX_ROOT}
+            PATHS ${XXX_SEARCH_PATHS}
             PATH_SUFFIXES lib lib64
+            DOC "XXX ${_comp} library"
     )
     if(XXX_${_comp}_LIBRARY)
         list(APPEND XXX_LIBRARIES ${XXX_${_comp}_LIBRARY})
@@ -49,19 +52,17 @@ endforeach()
 # Set include directories
 set(XXX_INCLUDE_DIRS ${XXX_INCLUDE_DIR})
 
-# Version check (adapted it with your own library structure)
-if(XXX_INCLUDE_DIR AND EXISTS "${XXX_INCLUDE_DIR}/../release-xxx.txt")
-    get_filename_component(XXX_PARENT_DIR ${XXX_INCLUDE_DIR} DIRECTORY)
-    set(RELEASE_FILE "${XXX_PARENT_DIR}/release-xxx.txt")
-    if(EXISTS "${RELEASE_FILE}")
-        file(READ "${RELEASE_FILE}" XXX_VERSION_CONTENTS LIMIT_COUNT 1)
-        string(REGEX MATCH "XXX SDK ([0-9]+\\.[0-9]+\\.[0-9]+)" XXX_VERSION_MATCH "${XXX_VERSION_CONTENTS}")
-        if(XXX_VERSION_MATCH)
-            string(REGEX REPLACE "XXX SDK ([0-9]+\\.[0-9]+\\.[0-9]+)" "\\1" XXX_VERSION "${XXX_VERSION_MATCH}")
-        else()
-            message(WARNING "Failed to detect XXX version from ${RELEASE_FILE}")
-        endif()
-    endif()
+# Version detection (customize based on your library's version format)
+if(XXX_INCLUDE_DIR AND EXISTS "${XXX_INCLUDE_DIR}/xxx_version.h")
+    file(STRINGS "${XXX_INCLUDE_DIR}/xxx_version.h" XXX_VERSION_MAJOR_LINE REGEX "^#define[ \t]+XXX_VERSION_MAJOR[ \t]+[0-9]+")
+    file(STRINGS "${XXX_INCLUDE_DIR}/xxx_version.h" XXX_VERSION_MINOR_LINE REGEX "^#define[ \t]+XXX_VERSION_MINOR[ \t]+[0-9]+")
+    file(STRINGS "${XXX_INCLUDE_DIR}/xxx_version.h" XXX_VERSION_PATCH_LINE REGEX "^#define[ \t]+XXX_VERSION_PATCH[ \t]+[0-9]+")
+
+    string(REGEX REPLACE "^#define[ \t]+XXX_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" XXX_VERSION_MAJOR "${XXX_VERSION_MAJOR_LINE}")
+    string(REGEX REPLACE "^#define[ \t]+XXX_VERSION_MINOR[ \t]+([0-9]+).*" "\\1" XXX_VERSION_MINOR "${XXX_VERSION_MINOR_LINE}")
+    string(REGEX REPLACE "^#define[ \t]+XXX_VERSION_PATCH[ \t]+([0-9]+).*" "\\1" XXX_VERSION_PATCH "${XXX_VERSION_PATCH_LINE}")
+
+    set(XXX_VERSION "${XXX_VERSION_MAJOR}.${XXX_VERSION_MINOR}.${XXX_VERSION_PATCH}")
 endif()
 
 # Handle the QUIETLY and REQUIRED arguments, set XXX_FOUND to TRUE if all listed variables are TRUE
@@ -70,6 +71,20 @@ find_package_handle_standard_args(XXX
         REQUIRED_VARS XXX_LIBRARIES XXX_INCLUDE_DIRS
         VERSION_VAR XXX_VERSION
 )
+
+# Create imported target
+if(XXX_FOUND AND NOT TARGET XXX::XXX)
+    add_library(XXX::XXX UNKNOWN IMPORTED)
+    set_target_properties(XXX::XXX PROPERTIES
+            IMPORTED_LOCATION "${XXX_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORIES "${XXX_INCLUDE_DIRS}"
+    )
+
+    # Optional: Add more properties if needed
+    # set_property(TARGET XXX::XXX PROPERTY
+    #     INTERFACE_COMPILE_DEFINITIONS XXX_SOME_DEFINITION
+    # )
+endif()
 
 # Mark variables as advanced
 mark_as_advanced(XXX_INCLUDE_DIR)
